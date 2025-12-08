0d7ba285a2e2125d32d31ff79eb879b59c2d4433
codereme <amansgz.ar@gmail.com>
2025-12-08 13:16:05 -0300

    fix: Article preview component
    - Improve js solution
    - Use the card footer toggle active class to show / hide social nav and
      author profile using css style
    - Remove unnecessary ids from html

--- article-preview-component/app.js
+++ article-preview-component/app.js
@@ -1,16 +1,7 @@
 const cardFooter = document.getElementById("cardFooter");
 const btnShare = document.getElementById("btnShare");
-const authorContent = document.getElementById("author");
-const socialNav = document.getElementById("socialNav");
 
 btnShare.addEventListener("click", () => {
-  /* Change the cardFooter background color:  
-  very dark grayish blue - white */
+  /* the css controls the elements author and social nav, childs of card-footer  */
   cardFooter.classList.toggle("active");
-
-  /* Hide / show the author content profile */
-  authorContent.classList.toggle("active");
-
-  /* Hide / show the social nav bar mobile - social tooltip desktop */
-  socialNav.classList.toggle("active");
 });
