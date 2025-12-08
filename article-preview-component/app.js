a7eb37be9f17cae23c1c635ca00804f997caeedf
codereme <amansgz.ar@gmail.com>
2025-12-08 13:04:04 -0300

    feat: Article preview component
    - Add ids in html for javascript
    - Improve js solution using active class
    - Add comments to script
    - Update classes for javascript

--- article-preview-component/app.js
+++ article-preview-component/app.js
@@ -1,6 +1,16 @@
+const cardFooter = document.getElementById("cardFooter");
 const btnShare = document.getElementById("btnShare");
-const barAuthor = document.getElementById("author");
+const authorContent = document.getElementById("author");
+const socialNav = document.getElementById("socialNav");
 
 btnShare.addEventListener("click", () => {
-  barAuthor.classList.toggle("author-show");
+  /* Change the cardFooter background color:  
+  very dark grayish blue - white */
+  cardFooter.classList.toggle("active");
+
+  /* Hide / show the author content profile */
+  authorContent.classList.toggle("active");
+
+  /* Hide / show the social nav bar mobile - social tooltip desktop */
+  socialNav.classList.toggle("active");
 });
