e64af5a23f5b03c002634e6da7873dfe64ae849a
codereme <amansgz.ar@gmail.com>
2025-12-21 14:55:36 -0300

    feat: Time tracking dashboard
    Add initial solution
    
    - Add starter files and design folder
    - Add fonts to assets
    - Add semantic html structure
    - Add style for components, layout and media queries
    - Add js with fetch to data json and render data cards
    - WIP: selector-buttons, grid and fonts
    - Add readme

---
+++ time-tracking-dashboard/js/app.js
@@ -0,0 +1,79 @@
+const content = document.getElementById("content");
+const selector = document.querySelector(".timeframe-selector");
+
+let selectedTimeframe = "weekly";
+let appData = [];
+
+selector.addEventListener("click", (e) => {
+  if (e.target.matches("button[data-timeframe]")) {
+    selectedTimeframe = e.target.dataset.timeframe;
+  }
+
+  if (appData.length > 0) {
+    updateCards();
+  } else {
+    console.log("no data");
+  }
+});
+
+const updateCards = () => {
+  content.innerHTML = "";
+
+  appData.forEach((item) => {
+    appendItem(item);
+  });
+};
+
+/* Create each card */
+const appendItem = (item) => {
+  const card = document.createElement("article");
+
+  const timeframeData = item.timeframes[selectedTimeframe];
+
+  let period;
+  if (selectedTimeframe === "daily") {
+    period = "Yesterday";
+  } else if (selectedTimeframe === "weekly") {
+    period = "Last Week";
+  } else {
+    period = "Last Month";
+  }
+
+  card.innerHTML = `
+    <div class="card card-${item.title.toLowerCase()}">
+      <div class="card__content">
+        <div class="row">
+          <h2 class="card__title">${item.title}</h2>
+          <img src="./assets/images/icon-ellipsis.svg" alt="" class="icon-menu" aria-hidden="true">
+        </div>
+
+        <div class="card__details">
+          <p  class="card__details--hours">${timeframeData.current} hrs</p>
+          <p class="card__details--period">${period} - ${
+    timeframeData.previous
+  } hrs</p>
+        </div>
+      </div>
+    </div>
+  `;
+
+  content.appendChild(card);
+};
+
+const populateDOM = (data) => {
+  appData = data;
+
+  appData.forEach((item) => {
+    appendItem(item);
+  });
+};
+
+fetch("./js/data.json")
+  .then((response) => {
+    if (!response.ok) return console.log("oops! Something went wrong");
+
+    return response.json();
+  })
+  .then((data) => {
+    populateDOM(data);
+  });
